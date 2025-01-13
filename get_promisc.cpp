5851699e760b16796832d9edd54755fac8fdbd4c
wyou <wentao.you@nokia-sbell.com>
2025-01-13 12:56:39 +0200

    promisc

---
+++ get_promisc.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#include <cerrno>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>      // For htons
+#include <linux/if_ether.h>  // For ETH_P_ALL
+
+int main() {
+    const char* interface_name = "macvlan1";
+    int sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (sockfd < 0) {
+        std::cerr << "Error opening socket: " << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy(ifr.ifr_name, interface_name, IFNAMSIZ);
+
+    // Get the current flags
+    if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0) {
+        std::cerr << "Error getting flags: " << strerror(errno) << std::endl;
+        close(sockfd);
+        return 1;
+    }
+
+    // Check if promiscuous mode is enabled
+    if (ifr.ifr_flags & IFF_PROMISC) {
+        std::cout << "Promiscuous mode is enabled on " << interface_name << std::endl;
+    } else {
+        std::cout << "Promiscuous mode is not enabled on " << interface_name << std::endl;
+    }
+
+    close(sockfd);
+    return 0;
+}
+
